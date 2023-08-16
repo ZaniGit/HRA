afcf3e9dc3fdbb7365cbc372cb62009256b65a81
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-16 19:53:09 -0300

    Create CreateEmploye.php

---
+++ app/Filament/Resources/EmployeResource/Pages /CreateEmploye.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\EmployeResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\EmployeResource;
+
+class CreateEmploye extends CreateRecord
+{
+    protected static string $resource = EmployeResource::class;
+}
