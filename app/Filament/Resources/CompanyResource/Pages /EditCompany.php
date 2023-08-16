ce81cd8b95df416da1407267cb5f6988c3f94e2d
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-16 19:48:28 -0300

    Create EditCompany.php

---
+++ app/Filament/Resources/CompanyResource/Pages /EditCompany.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\CompanyResource\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\CompanyResource;
+
+class EditCompany extends EditRecord
+{
+    protected static string $resource = CompanyResource::class;
