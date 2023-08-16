025df89bf8f1554bcaf86c43cd010a226adc3418
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-16 19:53:29 -0300

    Create EditEmploye.php

---
+++ app/Filament/Resources/EmployeResource/Pages /EditEmploye.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\EmployeResource\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\EmployeResource;
+
+class EditEmploye extends EditRecord
+{
+    protected static string $resource = EmployeResource::class;
+}
