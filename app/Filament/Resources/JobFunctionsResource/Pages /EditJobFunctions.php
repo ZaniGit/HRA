6c04eb4be7b0dc02d8e984c341c46d2518999233
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-16 22:05:46 -0300

    Create EditJobFunctions.php

---
+++ app/Filament/Resources/JobFunctionsResource/Pages /EditJobFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\JobFunctionsResource\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\JobFunctionsResource;
+
+class EditJobFunctions extends EditRecord
+{
+    protected static string $resource = JobFunctionsResource::class;
+}
