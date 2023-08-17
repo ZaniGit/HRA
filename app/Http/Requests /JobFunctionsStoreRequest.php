029d5dbcb1cfd7b60567cb1248322f9f94e984e8
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-17 00:00:59 -0300

    Create JobFunctionsStoreRequest.php

---
+++ app/Http/Requests /JobFunctionsStoreRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JobFunctionsStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'max:255', 'string'],
+            'salary' => ['required', 'numeric'],
+            'start_time' => ['required', 'date_format:H:i'],
+            'end_time' => ['required', 'date_format:H:i'],
+            'time_output_interval' => ['required', 'date_format:H:i'],
+            'time_entry_interval' => ['required', 'date_format:H:i'],
+        ];
+    }
+}
