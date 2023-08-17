6823ba401043cf0f1633c5c9271050a748fd1c1c
ZaniGit <115500318+ZaniGit@users.noreply.github.com>
2023-08-16 22:05:22 -0300

    Create CreateJobFunctions.php

---
+++ app/Filament/Resources/JobFunctionsResource/Pages /CreateJobFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\JobFunctionsResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\JobFunctionsResource;
+
+class CreateJobFunctions extends CreateRecord
+{
+    protected static string $resource = JobFunctionsResource::class;
+}
